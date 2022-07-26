9bb5e3211043c72e8781c74c659f0bf7e85e0374
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-07-26 20:52:05 +0100

    Lots of CSS and started the Readme

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,13 +5,13 @@ import final from "../assets/final.png"
 
 const Projects = () => {
     return (
-        <div class="my-projects">
+        <div className="my-projects">
         <h2>My Projects</h2>        
-        <div class="projects">
-  <div class="box box1"><img src={arcade} alt="arcade classic"/><div>Space Guardians</div></div>
-  <div class="box box2"><img src={frontend} alt="front-end"/><div>Front End</div></div>
-  <div class="box box3"><img src={backend} alt="back-end"/><div>Back End</div></div>
-  <div class="box box4"><img src={final} alt="arcade classic"/><div>Northcoders Final Project</div></div>
+        <div className="projects">
+  <div className="box box1"><img src={arcade} alt="arcade classic"/><div>Space Guardians</div></div>
+  <div className="box box2"><img src={frontend} alt="front-end"/><div>Front End</div></div>
+  <div className="box box3"><img src={backend} alt="back-end"/><div>Back End</div></div>
+  <div className="box box4"><img src={final} alt="arcade classic"/><div>Northcoders Final Project</div></div>
 </div>
 </div>
     )
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -9,11 +9,11 @@ function Skills() {
                 <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="The logo icon for react" title="React"/>
                 <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="The logo icon for HTML 5" title="HTML 5"/>
                 <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="The logo icon for CSS3" title="CSS 3"/>
-                <Skill source="https://upload.wikimedia.org/wikipedia/commons/1/1d/Cubase_logo.svg"alt="The logo icon for Cubase" title="Cubase"/>
                 <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="The logo icon for GitHub" title="GitHub"/>
                 <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="The logo icon for NPM" title="NPM"/>
                 <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="The logo icon for NodeJS" title="Node JS"/>
-                <Skill source={Phaser} alt="The logo icon for Cubase" title="Cubase"/>
+                <Skill source={Phaser} alt="The logo icon for Phaser" title="Phaser"/>
+                <Skill source="https://upload.wikimedia.org/wikipedia/commons/1/1d/Cubase_logo.svg" alt="The logo icon for Cubase" title="Cubase"/>
             </div>
             <p>I also have extensive experience as an IT engineer, able to configure and run Linux/Windows, and MacOS systems. </p>            
         </div>
