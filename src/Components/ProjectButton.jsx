f9dc7fc1c6bcd304b0a29b704e1876a195cbd68b
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-07-29 17:41:20 +0100

    Added a link button to try out

---
+++ src/Components/ProjectButton.jsx
@@ -0,0 +1,5 @@
+function ProjectButton({href, title}) {
+    return <a className="project-button" href={href} target="_blank" rel="noopener noreferrer">{title}</a>
+}
+
+export default ProjectButton;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,20 +1,35 @@
-import arcade from "../assets/arcade.png"
-import frontend from "../assets/frontend.png"
-import backend from "../assets/backend.png"
-import final from "../assets/final.png"
+import arcade from '../assets/arcade.png';
+import frontend from '../assets/frontend.png';
+import backend from '../assets/backend.png';
+import final from '../assets/final.png';
+import ProjectButton from './ProjectButton';
 
 const Projects = () => {
-    return (
-        <div className="my-projects">
-        <h2>My Projects</h2>        
-        <div className="projects">
-  <div className="box box1"><img src={arcade} alt="arcade classic"/><div>Space Guardians (Aquinta Mix)</div></div>
-  <div className="box box2"><img src={frontend} alt="front-end"/><div>Front End</div></div>
-  <div className="box box3"><img src={backend} alt="back-end"/><div>Back End</div></div>
-  <div className="box box4"><img src={final} alt="arcade classic"/><div>Northcoders Final Project</div></div>
-</div>
-</div>
-    )
-}
+  return (
+    <div className="my-projects">
+      <h2>My Projects</h2>
+      <div className="projects">
+        <div className="box box1">
+          <img src={arcade} alt="arcade classic" />
+          <div>Space Guardians (Aquinta Mix)
+            <ProjectButton href="https://github.com/EduardoAquinta/SpaceGuardiansAquintaMix" title="source code" />
+          </div>
+        </div>
+        <div className="box box2">
+          <img src={frontend} alt="front-end" />
+          <div>Front End</div>
+        </div>
+        <div className="box box3">
+          <img src={backend} alt="back-end" />
+          <div>Back End</div>
+        </div>
+        <div className="box box4">
+          <img src={final} alt="arcade classic" />
+          <div>Northcoders Final Project</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
