46757a70b764ea0ee06d9175e1999c6971b21305
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-07-29 20:39:54 +0100

    playing around with the burger menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import rune from '../assets/rune.png';
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            
-                <li className="navlist">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="navlist">
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li className="navlist">
-                    <Link to="/skills">Skills</Link>
-                </li>
-                <li className="navlist">
-                    <Link to="/contact">Contact</Link>
-                </li>
-            
-        </div>
-    );
-}
+
+    const [burgerOpen, setBurgerOpen] = useState(false);
+
+    const toggleBurger = () => {
+        setBurgerOpen(!burgerOpen);
+    }
+  return (
+    <div className="navbar">
+      <div>        
+        <img className="icon" src={rune} alt="rune" />
+      </div>
+      <li className="navlist" id="navi-list">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="navlist">
+        <Link to="/projects">Projects</Link>
+      </li>
+      <li className="navlist">
+        <Link to="/skills">Skills</Link>
+      </li>
+      <li className="navlist">
+        <Link to="/contact">Contact</Link>
+      </li>
+      <div className='menu' onClick={toggleBurger}>
+        <div className='menu-line'></div>
+        <div className='menu-line'></div>
+        <div className='menu-line'></div>
+        <style> {
+}               .navlist {`
+   
+    display: ${burgerOpen ? 'block' : 'none'};
+  `}</style>
+     
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
