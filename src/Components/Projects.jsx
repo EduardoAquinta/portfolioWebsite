0d21729ca685399bd8763197f6bc2856b57fa9c6
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-08-01 11:51:52 +0100

    generaly complete, need to finish codetags

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,12 +7,14 @@ import ProjectButton from './ProjectButton';
 const Projects = () => {
   return (
     <div className="my-projects">
-      <h2>My Projects</h2>
+      <h2 className="projects-title">My Projects</h2>
       <div className="projects">
         <div className="box box1">
           <img src={arcade} alt="arcade classic" />
           <div className='project-text'>
-            Space Guardians (Aquinta Mix)          </div>
+            <strong>Space Guardians (Aquinta Mix)</strong>
+            <div>A version of Space Guardians, taken out of the React frontend,  that has a local high score table.</div>
+            </div>
 
             <p>
               <ProjectButton href="" title="live app" />
@@ -24,7 +26,7 @@ const Projects = () => {
         </div>
         <div className="box box2">
           <img src={frontend} alt="front-end" />
-          <div className='project-text'>Front End</div>
+          <div className='project-text'><strong>Front End</strong><div>My Front-end project for Northcoders, using React, HTML, and CSS.</div></div>
           <p>
             <ProjectButton href="https://edenv2.netlify.app" title="live app"/>
             <ProjectButton
@@ -35,7 +37,7 @@ const Projects = () => {
         </div>
         <div className="box box3">
           <img src={backend} alt="back-end" />
-          <div className='project-text'>Back End</div>
+          <div className='project-text'><strong>Back End</strong><div>My Back-end project for Northcoders, using JS, Node, Express, Axios, and postGreSQL.</div></div>
           <p>
             <ProjectButton href="https://edenv1.herokuapp.com/api" title="live app" />
             <ProjectButton href="https://github.com/EduardoAquinta/backendAppV1" title="source code"/>
@@ -43,7 +45,7 @@ const Projects = () => {
         </div>
         <div className="box box4">
           <img src={final} alt="arcade classic" />
-          <div className='project-text'>Northcoders Final Project</div>
+          <div className='project-text'><strong>Northcoders Final Project</strong><div>My Final Project with Northcoders, in a team of 5, using React, Firebase, Phaser, HTML, CSS and Axios. I focused mainly on the Phaser coding and game logic.</div></div>
           <p>
             <ProjectButton href="https://spaceguardians-d5924.web.app/" title="live app" />
             <ProjectButton href="https://github.com/EduardoAquinta/spaceguardians" title="source code"/>
