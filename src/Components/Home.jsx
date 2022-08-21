b7c83c4477b76ed95611b887d3a557948c7debdf
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-08-21 20:55:13 +0100

    Tidied up the contact page and added some text to the home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,11 +7,12 @@ const Home = () => {
         <>
         <div className="home">
             <h2>Software Engineer in the making</h2>
-            <p className="about-me">I am a creative, articulate, and passionate junior software engineer. I have worked on a wide variety of projects in my life, from running Music Festivals to coding a Retro Arcade game. I am currently running self-generated projects after my time at a coding bootcamp and looking to land my first tech role. </p>
+            <p className="about-me">I am a creative, articulate, and passionate junior software engineer. I have worked on a wide variety of projects in my life, from running Music Festivals to coding a Retro Arcade game.<br></br> Currently I am running self-generated projects after my time at a coding bootcamp, looking to land my first tech role. </p>
             <p className="extra">Please feel free to have a look around my portfolio, and then contact me to dicuss whatever you would like!</p>
         </div>
         <div>
-         <p className="extra">This is a video of my talk at the Lightening Talk fest at NorthCoders Bootcamp, where I describe my take on the evolution of AI.</p>
+         <p className="extra">This is a video of my talk at the Lightening Talk fest at NorthCoders Bootcamp, where I describe my take on the evolution of AI.<br></br> 
+         I include this to shopw my capacity to break complex ideas into digestable chunks, and also because I think it is a wonderful topic to discuss!</p>
          <YoutubeEmbed embedId="y3B_RGxM4Nk?t=3710" />
        </div>
        </>
